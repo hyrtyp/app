40839282069ca2b7ca0752bece72a3249aad5591
hyrtyp <13718868826@163.com>
2014-01-10 18:26:15 +0800

    picture

--- src/main/java/com/hyrt/cnp/FullscreenActivity.java
+++ src/main/java/com/hyrt/cnp/FullscreenActivity.java
@@ -12,6 +12,8 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+import roboguice.inject.InjectView;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
