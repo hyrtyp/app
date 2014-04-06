5a6e3e0c57226d4c8d50b5ad2275a7c623299d2f
zrong0 <zoerong916@gmail>
2014-04-06 17:55:01 +0800

    change activityModule

--- src/main/java/com/hyrt/cnp/module/ActivityModule.java
+++ src/main/java/com/hyrt/cnp/module/ActivityModule.java
@@ -14,6 +14,7 @@ import com.hyrt.cnp.classroom.ui.ClassroomphotolistActivity;
 import com.hyrt.cnp.dynamic.ui.BabayIndexActivity;
 import com.hyrt.cnp.dynamic.ui.BabayWordActivity;
 import com.hyrt.cnp.dynamic.ui.DynamicCommentActivity;
+import com.hyrt.cnp.dynamic.ui.ForwardActivity;
 import com.hyrt.cnp.school.ui.ClassRoomListActivity;
 import com.hyrt.cnp.school.ui.SchoolCityListActivity;
 import com.hyrt.cnp.school.ui.SchoolIndexActivity;
@@ -175,4 +176,7 @@ public class ActivityModule extends AbstractModule {
     @Provides
     @Named("schoolCityListActivity")
     Class schoolCityListActivity() {return SchoolCityListActivity.class;}
+    @Provides
+    @Named("forwardActivity")
+    Class forwardActivity() {return ForwardActivity.class;}
 }
